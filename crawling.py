4474631730ce72bd4298c18406fcfc67995e69cc
Jongkyung Kim <75403001+tilthend@users.noreply.github.com>
2021-03-27 11:22:48 +0900

    Add files via upload

---
+++ crawling.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#old_content > table > tbody > tr')
+points = soup.select('#old_content > table > tbody > tr')
+
+#old_content > table > tbody > tr:nth-child(3) > td:nth-child(1) > img
+#old_content > table > tbody > tr:nth-child(4) > td:
+for tr in trs:
+    a_tag = tr.select_one('td.title > div > a')
+    if a_tag is not None:
+        rank = tr.select_one('td:nth-child(1) > img')['alt']
+        title = a_tag.text
+        stars = tr.select_one('td.point').text
+        print(rank, title, stars)
