e834322720b32a2decdb71eb6756f689a35903ac
Jongkyung Kim <75403001+tilthend@users.noreply.github.com>
2021-04-05 23:39:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/test', methods=['GET'])
-def test_get():
-   title_receive = request.args.get('title_give')
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+## API 역할을 하는 부분
+@app.route('/review', methods=['POST'])
+def write_review():
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
 
-@app.route('/mypage')
-def mypage():
-   return 'mypage 입니다!'
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
 
-@app.route('/test', methods=['POST'])
-def test_post():
-   title_receive = request.form['title_give']
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+    db.bookreview.insert_one(doc)
+
+    return jsonify({'msg': '저장완료'})
+
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': '이 요청은 GET!'})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
